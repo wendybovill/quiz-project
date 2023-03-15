8f37eabefe9857ae75d112f2f7457b930ba881d8
wendybovill <wendybovill@gmail.com>
2023-03-15 18:04:34 +0000

    correcting errors on loading questions

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -19,15 +19,52 @@ let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
-let questions = [];
+let questions =[{
+    question: "Inside which HTML element do we put the JavaScript??",
+    option1: "<script>",
+    option2: "<javascript>",
+    option3: "<js>",
+    option4: "<scripting>",
+    answer: 1
+  },
+  {
+    question:
+      "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    option1: "<script href='xxx.js'>",
+    option2: "<script name='xxx.js'>",
+    option3: "<script src='xxx.js'>",
+    option4: "<script file='xxx.js'>",
+    answer: 3
+  },
+  {
+    question: " How do you write 'Hello World' in an alert box?",
+    option1: "msgBox('Hello World');",
+    option2: "alertBox('Hello World');",
+    option3: "msg('Hello World');",
+    option4: "alert('Hello World');",
+    answer: 4
+  }
+];
 
 
+/*fetch('questions.json')
+    .then((result) => {
+        return result.json();
+    })
+    .then((loadedQuestions) => {
+        questions = loadedQuestions;
+        startQuiz();
+    })
+    .catch((error) => {
+        console.error(error);
+    });*/
 
+// set bonus for correct questions and set max questions
 const BONUS = 10;
 const MAX_QUESTIONS = 4;
 
 // Declaring first function to start the quiz with 0 counter, scores etc.
-start_quiz = () => {
+startQuiz = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
