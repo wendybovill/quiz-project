7afc85115f5261e1f80cc14dc19570095850e727
wendybovill <wendybovill@gmail.com>
2023-03-15 13:44:12 +0000

    added comments to js file

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -6,15 +6,7 @@
  * All functions are found in here. Without this file the quiz will not work.
  */
 
-let quiz = {
-    currentGame: [],
-    playerMoves: [],
-    score: 0,
-    turnNumber: 0,
-    lastButton: "",
-    turnInProgress: false,
-    choices: ["option1", "option2", "option3", "option4"]
-};
+// Declaring initial variables
 
 const question = document.getElementById('question');
 const options = Array.from(document.getElementById('options'));
@@ -24,21 +16,21 @@ let acceptingAnswers = true;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
-
 let questions = [];
 
 const BONUS = 10;
 const MAX_QUESTIONS = 4;
 
+// Declaring first function to start the quiz with 0 counter, scores etc.
 start_quiz = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
     console.log(availableQuestions);
-    getNewQuestion();
+    getNextQuestion();
 };
 
-getNewQuestion = () => {
+getNextQuestion = () => {
 
     questionCounter++;
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
@@ -50,6 +42,9 @@ getNewQuestion = () => {
         option.innerText = currentQuestion["option" + number];
     });
 
+    availableQuestions.splice(questionIndex, 1);
+
+    acceptingAnswers = true;
 }
 
 startQuiz();
