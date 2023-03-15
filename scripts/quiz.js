f45b4d858a48c0f9385ea98e669f35524c19af7d
Pix <pixilchic@live.co.uk>
2023-03-15 19:37:23 +0000

    edited question variable content

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -19,31 +19,7 @@ let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
-let questions = [{
-        question: "Inside which HTML element do we put the JavaScript??",
-        option1: "<script>",
-        option2: "<javascript>",
-        option3: "<js>",
-        option4: "<scripting>",
-        answer: 1
-    },
-    {
-        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        option1: "<script href='xxx.js'>",
-        option2: "<script name='xxx.js'>",
-        option3: "<script src='xxx.js'>",
-        option4: "<script file='xxx.js'>",
-        answer: 3
-    },
-    {
-        question: " How do you write 'Hello World' in an alert box?",
-        option1: "msgBox('Hello World');",
-        option2: "alertBox('Hello World');",
-        option3: "msg('Hello World');",
-        option4: "alert('Hello World');",
-        answer: 4
-    }
-];
+let questions = [{}];
 
 
 fetch('questions.json')
