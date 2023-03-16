8b21bce4bc696dc2a45ea00cdf298114f8be957b
Pix <pixilchic@live.co.uk>
2023-03-16 14:44:58 +0000

    to local storage set clear scores to remove scores set text to display when there are no user scores saved

--- scripts/remove_user_score.js
+++ scripts/remove_user_score.js
@@ -1,34 +1,11 @@
 /* Get username, the users final score,  save the score, and retrieve the latest scores */
 const username = document.getElementById('username');
-const saveScore = document.getElementById('saveScore');
+const clearScore = document.getElementById('clearScore');
 const finalScore = document.getElementById('finalScore');
-const latestScore = localStorage.getItem('latestScore');
-/* Retrieve the top scores from local storage */
-const topScores = JSON.parse(localStorage.getItem('topScores')) || [];
-/* How many scores to display? */
-const MAX_TOP_SCORES = 6;
+const clearItems = localStorage.getItem('topScore');
 
-/* To display the latest score for the user */
-finalScore.innerText = latestScore;
 
-/* If no username is entered leave saveScore button disabled */
-username.addEventListener('keyup', () => {
-        saveScore.disabled = !username.value;
-    })
-    /* When the user enters their username, allow save button to be active - removed disabled */
-saveTopScore = (e) => {
-    e.preventDefault();
-    const score = {
-        score: latestScore,
-        user: username.value
-    };
-
-    topScores.push(score);
-    topScores.sort((a, z) => z.score - a.score); //sort Z to A with the newest Z on top and the older A on bottom
-    topScores.splice(6);
-
-    /* Save topscores to local storage in a JSON string */
-    localStorage.setItem('topScores', JSON.stringify(topScores));
-    window.location.assign('/');
-
-};
+/* When the user enters their username, allow clear button to be active - removed disabled */
+clearScores = (e) => {
+    window.localStorage.clear(clearItems);
+}
--- scripts/topscores.js
+++ scripts/topscores.js
@@ -5,9 +5,15 @@
 //Either retrieve the topscores or an empty array
 const listTopScores = document.getElementById('listTopScores');
 const topScores = JSON.parse(localStorage.getItem('topScores')) || [];
+const allScores = JSON.parse(localStorage.getItem('topScores'));
 
-listTopScores.innerHTML = topScores.map(score => {
-    return '<li><span style="font-style: bold; font-size: 2rem;">' + `${score.user}   ${score.score}` + '</span></li>';
-})
 
-.join('');
+if (allScores == '') {
+    listTopScores.innerHTML = '<li><span style="font-style: bold; font-size: 2rem;">There are no scores available. Have a go at the quiz and save your score.</sapn></li>';
+
+} else {
+    listTopScores.innerHTML = topScores.map(score => {
+            return '<li><span style="font-style: bold; font-size: 2rem;">' + `${score.user}   ${score.score}` + '</sapn></li>';
+        })
+        .join('');
+}
